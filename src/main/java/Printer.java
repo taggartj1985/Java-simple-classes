b69b0389df1ecd5d0c0bc6c0647cb676b7683714
John Taggart <taggartj1985@gmail.com>
2020-05-25 16:56:34 +0100

    mvp done

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,13 @@
 public class Printer {
+
+    private int toner;
+    private int paper;
 }
+
+
+//    Create a Printer class that has a property for number of sheets of paper left.
+//        Add a method to print that takes in a number of pages and number of copies.
+//        The print method will only run if the printer has enough paper.
+//        If it runs it will reduce the value of the paper left by number of copies * number of pages.
+//        Add a toner volume property to the class.
+//        Modify the printer so that it reduces the toner by 1 for each page printed.
