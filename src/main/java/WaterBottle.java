e9b4d69b10d193a32bb0c1faa8b35865e0a7b234
John Taggart <taggartj1985@gmail.com>
2020-05-25 16:51:57 +0100

    waterbottle methods and tests working

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,38 @@
 public class WaterBottle {
+
+    private int volume;
+
+    public WaterBottle(){
+        this.volume = 100;
+    }
+
+    public int volume(){
+        return this.volume;
+    }
+
+    public int takeDrink(){
+        return this.volume - 10;
+    }
+
+    public int fillBottle(){
+        this.volume = 100;
+        return this.volume;
+    }
+    public int emptyBottle(){
+        this.volume = 0;
+        return this.volume;
+    }
+
+
+
+
+
+
 }
+
+//    Water Bottle
+//    Create a water bottle class with a volume property.
+//        The volume should start at 100.
+//        Add a drink function that takes 10 from the volume each time it is called.
+//        Create an empty function that brings the volume down to 0.
+//        Create a fill function that fills the volume back to 100.
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    WaterBottle bottle;
+
+    @Before
+        public void before(){
+        bottle = new WaterBottle();
+    }
+
+    @Test
+        public void getVolume(){
+        assertEquals(100, bottle.volume());
+    }
+
+    @Test
+    public void canDrink(){
+        assertEquals(90, bottle.takeDrink());
+    }
+
+    @Test
+    public void canFill(){
+        assertEquals(100, bottle.fillBottle());
+    }
+
+    @Test
+    public void canEmpty(){
+        assertEquals(0, bottle.emptyBottle());
+    }
+
 }
