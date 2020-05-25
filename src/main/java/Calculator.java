026ea614a6ad9b6919c6dc6d9b176cb07a8efefc
John Taggart <taggartj1985@gmail.com>
2020-05-25 16:36:54 +0100

    calculator methods and tests working

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,21 @@
 public class Calculator {
+
+    public int add(int numberOne, int numberTwo){
+        return (numberOne + numberTwo);}
+
+    public int subtract(int numberOne, int numberTwo){
+        return (numberOne - numberTwo);}
+
+    public int multiply(int numberOne, int numberTwo){
+        return (numberOne * numberTwo);}
+
+    public double divide(double numberOne, double numberTwo){
+        return (numberOne / numberTwo);}
+
 }
+
+
+
+//Create a Calculator class. This should have functions for Add, Subtract, Multiply and Divide.
+//        Your methods should take in two ints to perform the calculations on
+//        (except the Divide method. This should take two doubles as arguments).
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+
+    @Test
+        public void canAdd(){
+        assertEquals(15, calculator.add(10,5));
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(5, calculator.subtract(10,5));
+    }
+
+    @Test
+    public void canMultiply(){
+        assertEquals(50, calculator.multiply(10,5));
+    }
+
+    @Test
+    public void canDivide(){
+        assertEquals(2, calculator.divide(10,5), 0.1);
+    }
+
 }
